0353e342a4d0717d2115627478b021fb15285c34
HakeemFS <andersonhakeem@gmail.com>
2015-07-09 19:34:28 -0400

    Learning Variables
    Learning Variables

--- Day 2/js/script.js
+++ Day 2/js/script.js
@@ -17,3 +17,20 @@ Anything between the stars and slashes is ignored by the browsers
 
 //Alert - gives the user information but returns nothing to us
 alert("Welcome to SDI!");
+
+//Console Logging information
+console.log("This is the console. Learn to love it!");
+
+//Declare a variable
+//Start with the keyword var
+var whatever; //Declaring of a variable
+//Define the variable
+whatever = 45;
+// Create a storage container = value ;
+
+//Declare and define a variable
+var a =2;
+
+//Send variable value to the console so we can see it
+console.log(a);
+
