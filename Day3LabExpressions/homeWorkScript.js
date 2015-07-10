633a4863d62e5ed410ab8d8bd7ba1c998126ecba
HakeemFS <andersonhakeem@gmail.com>
2015-07-09 22:50:46 -0400

    Slices of Pie part 1
    Part 1 end result

---
+++ Day3LabExpressions/homeWorkScript.js
@@ -0,0 +1,27 @@
+/**
+ * Hakeem Anderson
+ * 07-09-2015
+ * SDI
+ * 00
+ */
+
+    alert("Day 3 Lab Expressions")
+
+//Dividing Pizza
+//Slices of Pizza per box
+var slices = 9;
+
+//Number of people at the party
+var people = 11;
+
+//Number of Pizzas ordered
+var boxesOrdered =3;
+
+//Number of slices per person (SPP)
+
+//Slices * boxesOrdered/People
+var SPP = slices * boxesOrdered/people;
+console.log("The number of slices per person "+SPP);
+
+
+
