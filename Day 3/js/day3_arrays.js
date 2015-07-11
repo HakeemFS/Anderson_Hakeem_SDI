ddc8a14fe3ef409bbbb0eec4823c4ca23af5921a
HakeemFS <andersonhakeem@gmail.com>
2015-07-11 18:55:59 -0400

    Pop, Push, Splice
    Rearranging arrays

--- Day 3/js/day3_arrays.js
+++ Day 3/js/day3_arrays.js
@@ -23,3 +23,51 @@ console.log(orangeBins.length);
 
 
 console.log("The average number of oranges picked is "+averageOranges);
+
+//Create an array of Avengers
+var avengerNames = ["Iron-man", "Captian America", "Hawkeye", "Hulk"];
+
+//Print
+console.log(avengerNames);
+
+//Access a specific item in the array
+console.log(avengerNames[1]);
+
+//Use a variable as an index number
+var num = 2;
+console.log(avengerNames[num]);
+
+//Set a specific item
+avengerNames[3]="Thor";
+console.log(avengerNames);
+
+//Add a new item in out array
+avengerNames[4] = "Falcon";
+console.log(avengerNames);
+
+//Add a new item
+avengerNames[avengerNames.length] = "Black Widow";
+console.log(avengerNames);
+
+//Create new array for fruits
+var fruitArray = ["banana", "pear", "peach", "strawberry"];
+console.log(fruitArray);
+
+//Push - method or a function of arrays
+//Adds it to the end of the array
+fruitArray.push("pineapple");
+console.log(fruitArray);
+console.log(fruitArray[4]);
+
+fruitArray.push("date");
+console.log(fruitArray);
+
+//Pop method - removes the last item in the array And returns it
+var caught = fruitArray.pop();
+console.log(fruitArray);
+console.log(caught);
+
+//splice - method used for adding and removing items from array
+//splice(position, # of items to remove, items to put in)
+fruitArray.splice(0,0,"lemmon","cherry");
+console.log(fruitArray);
