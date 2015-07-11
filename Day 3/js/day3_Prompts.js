b398385c8c92d65af613821aae592128e3bfb52d
HakeemFS <andersonhakeem@gmail.com>
2015-07-11 19:20:02 -0400

    Prompts commands
    Results, Alerts

--- Day 3/js/day3_Prompts.js
+++ Day 3/js/day3_Prompts.js
@@ -5,6 +5,28 @@ Section 00
 Prompts
  */
 
-
 //alert("Testing to see if this works!");
 
+//Ask the user for their input
+var userInput = prompt("Enter your year of birth:");
+console.log(userInput);
+
+var age = 2015-userInput;
+console.log("You are "+age+" years old.");
+
+//Calculate the area of a rectangle
+
+//Prompt the user for width
+var width = prompt("Lets calculate the area of a rectangle.\nPlease enter a width:");
+
+//Prompt the user for a height
+var height = prompt("Please enter a height:");
+
+//Calculae the area using the user input
+var area = width * height;
+console.log("The area of your rectangle is "+area);
+
+//Create a variable that holds the whole result
+var result = "area of your rectangle is "+area;
+console.log(result);
+alert(result);
