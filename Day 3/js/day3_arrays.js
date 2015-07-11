fb66052b6eb33153f5b344f788f04ebc7678b4e6
HakeemFS <andersonhakeem@gmail.com>
2015-07-11 18:03:19 -0400

    Arrays cont
    Addition of array

--- Day 3/js/day3_arrays.js
+++ Day 3/js/day3_arrays.js
@@ -5,4 +5,21 @@ Section 00
 Arrays
  */
 
-alert("Arrays");
+//alert("Arrays");
+
+//Create a basic array
+//Picking Oranges
+var orangeBins = [30,500,10002];
+
+//How many oranges did we pick?
+var totalOranges = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(totalOranges+" oranges were picked in 3 days.");
+
+//Average numbers of oranges picked
+var averageOranges = totalOranges/orangeBins.length;
+
+//Length property of arrays
+console.log(orangeBins.length);
+
+
+console.log("The average number of oranges picked is "+averageOranges);
