0ed5b3b3bd50aff89391b01f3c3f0d4b82ad6647
HakeemFS <andersonhakeem@gmail.com>
2015-07-11 23:45:24 -0400

    Adding Prompts
    Prompts and content for prompts

---
+++ Anderson_Hakeem_Expression_Assignment/ExpressionAssignment.js
@@ -0,0 +1,19 @@
+/*
+Hakeem Anderson
+Section 00
+7-11-15
+Expression Assignment
+ */
+
+
+//alert("Testing to see if this works");
+
+//Calculating average points per quarter through 4 quarters of basketball
+//
+
+
+//Points by quarter
+var pointsScoredQuarterOne = prompt("Enter your points scored in the 1st quarter");
+var pointsScoredQuarterTwo = prompt("Enter your points scored in the 2nd quarter");
+var pointsScoredQuarterThree = prompt("Enter your points scored in the 3rd quarter");
+var pointsScoredQuarterFour = prompt("Enter your points scored in the 4th quarter");
