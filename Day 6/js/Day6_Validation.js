c6e58251dde97fe1eda033f9ad0b534916508774
HakeemFS <andersonhakeem@gmail.com>
2015-07-18 20:58:37 -0400

    re-prompt user validation
    Re-Prompt for validation

--- Day 6/js/Day6_Validation.js
+++ Day 6/js/Day6_Validation.js
@@ -20,4 +20,23 @@ Topic Validating
  //(item.toLowerCase() != "yes" && item.toLowerCase() != "no"
 
  //For validating we use a while loop
- //
+ //Use a while loop because we do not know how many times they will get it wrong
+
+ //Ask for a number
+ var userNum = prompt("Please enter a number.");
+ //Directly after each prompt, validate for that prompt
+ while(isNaN(userNum) || userNum===""){
+  //re-propt using the same variable
+  userNum=prompt("Please only user numbers and dont leave blank.\nPlease type in a number.");
+ }
+ console.log("After the while loop.");
+
+ //Ask the user for red or black
+ var color=prompt("Please choose red or black:");
+ //validate
+while(color.toLocaleLowerCase() !="red" && color.toLowerCase() !="black"){
+ // re-prompt the user
+ color=prompt("Please only type in red or black.\nPlease choose your color.");
+
+}
+ 
