83f31d15334975d8dece8e0429ddfecd9fce72e5
HakeemFS <andersonhakeem@gmail.com>
2015-07-18 20:44:58 -0400

    Validating
    Validating loops

---
+++ Day 6/js/Day6_Validation.js
@@ -0,0 +1,23 @@
+
+ /*
+Hakeem Anderson
+Section 00
+Topic Validating
+7-18-15
+ */
+
+
+//alert("Testing to see if this works");
+
+
+ //isNaN(Item to test)
+ //true if not a number
+ //false if it is a number
+
+ //To test blank we test (item ==="")
+
+ //To test test strings
+ //(item.toLowerCase() != "yes" && item.toLowerCase() != "no"
+
+ //For validating we use a while loop
+ //
--- Day 6/js/Day6_for_loops.js
+++ Day 6/js/Day6_for_loops.js
@@ -29,4 +29,3 @@ For Loops
  }
 
 
- 
