b74c060215f6a24ad23ca1e4b0d069d4cc1edf09
HakeemFS <andersonhakeem@gmail.com>
2015-07-18 20:32:40 -0400

    For Loops
    Basic For Loops

---
+++ Day 6/js/Day6_for_loops.js
@@ -0,0 +1,18 @@
+
+ /*
+Hakeem Anderson
+Section 00
+7-18-2015
+For Loops
+ */
+
+
+//alert("Testing to see if this works");
+
+//Basic for loop
+ //for(intilize counter; condition; increment of change){}
+
+ for(var i=0; i<20; i+=4){
+  console.log("The value of i is "+i);
+
+ }
