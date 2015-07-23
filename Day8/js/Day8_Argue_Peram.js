e419b87739319f1d8e3ae46c76746ea6e18dc93d
HakeemFS <andersonhakeem@gmail.com>
2015-07-23 19:00:49 -0400

    Old school way
    Super specific function only will calculate the area of a rectangle
    10x20

--- Day8/js/Day8_Argue_Peram.js
+++ Day8/js/Day8_Argue_Peram.js
@@ -9,4 +9,26 @@ Arguments & Parameter's
 
 alert("Testing to see if this works");
 
- 
+ //Arguments go in the function calls and these are the specific values
+ //Perameters go in the fucntion definetion and cath the argements - Generic Containters
+
+ //Create var for width and height
+ var width = 10;
+ var height = 20;
+
+ //Create a funtion to calculate the area of a rectangle
+
+ function calcArea(){
+  //Create car
+  var width = 10;
+  var height = 20;
+  //calculate
+  var area = width*height;
+  //console
+  console.log("The area of a rectangle with a width of "+width+" and height of "+height+"is "+area);
+
+ }
+
+ //call the funciton
+ calcArea();
+ calcArea();
