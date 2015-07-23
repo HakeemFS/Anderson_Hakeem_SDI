ee1beb8095cee702252a928a1fd1b85da20e1a0a
HakeemFS <andersonhakeem@gmail.com>
2015-07-23 19:25:43 -0400

    Putting info into functions
    calculating dog years

--- Day8/js/Day8_Argue_Peram.js
+++ Day8/js/Day8_Argue_Peram.js
@@ -18,20 +18,55 @@ Arguments & Parameter's
 
  //Create a funtion to calculate the area of a rectangle
 
- function calcArea(){
+ function calcArea(w,h){
   //Create car
    //width = 10;
    //height = 20;
   //calculate
-  var area = width*height;
+  var area = w*h;
   //console
   console.log("The area of a rectangle with a width of "+width+" and height of "+height+"is "+area);
 
  }
 
  //call the function
- calcArea();
- //change the var
- width = 50;
- height = 10;
- calcArea();
+ calcArea(width,height);
+
+ //Find the area with width = 6 height = 7
+ calcArea(6,7);
+
+ //Ask the user
+ var userWidth = prompt("Lets calculate the are of a rectangle\nPlease enter a width");
+
+ //validate & number cast imput
+ var userHeight = prompt("Pleae enter a height:");
+
+ //How would I get the area
+ calcArea(userWidth,userHeight);
+
+ //Create a funciton that will calculate dog years
+
+ function dogYears(humanAge){
+  //Input a human year and spit out dog year
+  //HumanAge*7 dog age
+  var dogAge = humanAge*7;
+
+  //aprint
+  console.log("A dog who is "+humanAge+" is "+dogAge+" in dog years.")
+
+
+ }
+
+ //Calculate how old a 4 year old dog is in dog years
+
+ dogYears(4);
+
+ //Calculate a 7 year old dog
+ dogYears(7);
+
+ //Prompt the user for their dog age
+ var yourDogAge = prompt("Lets figure out how old your dog is in dog years.\nPlease enter your dogs's human age.");
+
+ //validate
+
+ dogYears(yourDogAge);
