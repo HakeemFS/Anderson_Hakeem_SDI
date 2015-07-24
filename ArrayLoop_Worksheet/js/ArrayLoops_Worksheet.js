c467b760e284677247ced5d76cde3dd0ce1d4f8b
HakeemFS <andersonhakeem@gmail.com>
2015-07-23 23:18:14 -0400

    Adding Alert
    Finish Loop and adding alert.

--- ArrayLoop_Worksheet/js/ArrayLoops_Worksheet.js
+++ ArrayLoop_Worksheet/js/ArrayLoops_Worksheet.js
@@ -13,14 +13,29 @@ Array Loops Worksheet
 
  var callsTakenWeekOne = parseInt(prompt("How many calls did you take in week one?"));
  while(isNaN(callsTakenWeekOne) || callsTakenWeekOne===""){
-  callsTakenWeekOne=prompt("Do not leave blank\nUse only numeric values\nPlease enter a your number of calls for week one.");
+  callsTakenWeekOne=prompt("Do not leave blank\nUse only numeric values\nPlease enter your number of calls for week one.");
  }
   var callsTakenWeekTwo = parseInt(prompt("How many calls did you take in week two?"));
  while(isNaN(callsTakenWeekTwo) || callsTakenWeekTwo===""){
-  callsTakenWeekTwo=prompt("Do not leave blank\nUse only numeric values\nPlease enter a your number of calls for week two.");
+  callsTakenWeekTwo=prompt("Do not leave blank\nUse only numeric values\nPlease enter your number of calls for week two.");
  }
   var callsTakenWeekThree = parseInt(prompt("How many calls did you take in week three?"));
   while(isNaN(callsTakenWeekThree) || callsTakenWeekThree===""){
-   callsTakenWeekThree=prompt("Do not leave blank\nUse only numeric values\nPlease enter a your number of calls for week three.");
+   callsTakenWeekThree=prompt("Do not leave blank\nUse only numeric values\nPlease enter your number of calls for week three.");
   }
 
+//Adding an Array
+
+ var totalCalls = [callsTakenWeekOne,callsTakenWeekTwo,callsTakenWeekThree];
+ var total=0;
+
+
+ for (var i=0; i<totalCalls.length;i++){
+  total+=totalCalls[i];
+ }
+
+
+
+  //Console log value
+console.log("The total calls made is " +total);
+ alert("The total calls made is "+total);
