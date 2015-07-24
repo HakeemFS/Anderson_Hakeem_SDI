83baf3b7d437698817f5b1fbea87b55aa0e3bff7
HakeemFS <andersonhakeem@gmail.com>
2015-07-23 22:06:35 -0400

    Prompt validation added
    Validating

--- ArrayLoop_Worksheet/js/ArrayLoops_Worksheet.js
+++ ArrayLoop_Worksheet/js/ArrayLoops_Worksheet.js
@@ -9,17 +9,18 @@ Array Loops Worksheet
 
 //alert("Testing to see if this works");
 
- //Creating a variable
+ //Creating a variable & Validating the prompt
 
  var callsTakenWeekOne = parseInt(prompt("How many calls did you take in week one?"));
  while(isNaN(callsTakenWeekOne) || callsTakenWeekOne===""){
-  if
+  callsTakenWeekOne=prompt("Do not leave blank\nUse only numeric values\nPlease enter a your number of calls for week one.");
  }
   var callsTakenWeekTwo = parseInt(prompt("How many calls did you take in week two?"));
  while(isNaN(callsTakenWeekTwo) || callsTakenWeekTwo===""){
-
+  callsTakenWeekTwo=prompt("Do not leave blank\nUse only numeric values\nPlease enter a your number of calls for week two.");
  }
   var callsTakenWeekThree = parseInt(prompt("How many calls did you take in week three?"));
   while(isNaN(callsTakenWeekThree) || callsTakenWeekThree===""){
-   
+   callsTakenWeekThree=prompt("Do not leave blank\nUse only numeric values\nPlease enter a your number of calls for week three.");
   }
+
