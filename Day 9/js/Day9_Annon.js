d5719fe7a8707680b6f71b19e268b636253bb6f9
HakeemFS <andersonhakeem@gmail.com>
2015-07-25 19:24:32 -0400

    Using Anons
    Annons & var

--- Day 9/js/Day9_Annon.js
+++ Day 9/js/Day9_Annon.js
@@ -9,4 +9,32 @@ Annon
 
 //alert("Testing to see if this works");
 
- //
+ //Basic Annonymous fuction
+ //var functionName = function(){};
+ //Extreamly popular in JS & JQuery
+ //Important in object oriented programming
+ //For now, it is matter style choice between anon and regular
+
+ //WILL NOT WORK!!!
+ //Test function call
+ //var results2 = triArea(5,10);
+ //console.log(results2);
+
+
+
+ //Create an anon funciton to calculate the area of a triangle
+
+ var triArea = function(b,h){
+  var area = 5*b*h;
+  return area;
+
+
+
+
+ };
+
+ //Function call for a non funciton
+
+ var results = triArea(10,20);
+
+ console.log(results);
