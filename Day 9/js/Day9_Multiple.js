81e2ac34634e49cef9e3f535e95b4b3b15258b4e
HakeemFS <andersonhakeem@gmail.com>
2015-07-25 20:07:44 -0400

    If Statement
    Added if statement to conditional

--- Day 9/js/Day9_Multiple.js
+++ Day 9/js/Day9_Multiple.js
@@ -14,13 +14,38 @@ Multi
  var height = 8;
  var width = 6;
 
- //create another varible for a base of a triangle
+ //create another variable for a base of a triangle
  var base = 5;
 
 
  //Function call to area
  var resultsArea = calcArea(height,width);
  console.log(resultsArea);
+
+
+ //Ask the user if they would like to calculate the area of a triangele
+ var userPrompt=prompt("Would you like to calculate the area of a triangle?\nPlease answer yes or no.");
+ //Validate
+ while(userPrompt.toLowerCase() !="yes" && userPrompt.toLowerCase() !="no"){
+  userPrompt=prompt("Please only answer yes or no.\nWould you like to calculate the area of a triangle?");
+
+ }
+
+ //conditioonal to see if we should run the function
+ if(userPrompt.toLowerCase()==="yes"){
+  var triArea = function(b,h){
+   var area = .5*b*h;
+   return area;};
+//Function call to triangle area
+  var triAreaTotal = triArea(base,height);
+  console.log(triAreaTotal);
+
+  }
+  
+
+
+
+
 
  //Create a function to calculate the perimiter
 
